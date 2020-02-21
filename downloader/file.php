da2b9bdf7f0545c15f30a25b5a18ef7937e55826
Yaroslav-Ivanov <yaroslavvitebsk@gmail.com>
2020-02-21 20:41:50 +0300

    downloader

---
+++ downloader/file.php
@@ -0,0 +1,6 @@
+<?php
+if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    copy($_FILES['userfile']['tmp_name'], "file\\" . $_FILES['userfile']['name']);
+}
+
+header("Location: file2.php"); 
---
+++ downloader/file2.php
@@ -0,0 +1,20 @@
+<?php
+$arr = scandir("file");
+
+
+for ($i = 2; $i < count($arr); $i++) {
+
+    switch (explode(".", $arr[$i])[1]) {
+        case 'docx';
+            $icon = 'docx-file-14-504256.png';
+            break;
+        case 'pdf';
+            $icon = 'adobe-pdf-icon.png';
+            break;
+        default:
+            $icon = 'images.png';
+            break;
+    }
+    echo "<a href='file\\$arr[$i]'><img src='$icon' width='26' height='26'>$arr[$i]</a> <a href='del.php'><img src='cross.png' width='32' height='32'></a> <br>";
+}
+
